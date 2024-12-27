81f798a8c319240bd67daba61707914ec1374f21
Otasmacour <macourekota7@gmail.com>
2024-12-27 19:34:15 +0100

    Boruvka

--- Grafy/Grafy.py
+++ Grafy/Grafy.py
@@ -7,11 +7,18 @@ def PrintResultOfMinimumSpanningTree(edges, algorithmName):
     print("With a total length of "+str(totalLength))
     print()
 
+def ShortestEdge(edges):
+    shortest = edges[0]
+    for edge in edges:
+        if(edge.length < shortest.length):
+            shortest = edge
+    return shortest    
+
 def PrintGraph(graph):
-    for node in graph.nodes:
-        print("Node "+str(node.index))
-        for key in node.adjacentVerticesAndDistances: 
-            print("To "+str(key)+", length:"+str(node.adjacentVerticesAndDistances[key]))
+    for vertex in graph.edges:
+        print("Vertex "+str(vertex.index))
+        for key in vertex.adjacentVerticesAndDistances: 
+            print("To "+str(key)+", length:"+str(vertex.adjacentVerticesAndDistances[key]))
 
 def NextDecimalNumber(string):
     result = 0
@@ -139,11 +146,14 @@ class PriorityQueueShortestEdges():
             self.start = self.start.next
             return node
 
-class Node():
+class Vertex():
 
     def __init__(self, index):
         self.index = index
         self.edges = []
+    
+    def ShortestEdge(self):
+        return ShortestEdge(self.edges)
 
 class Edge():
     
@@ -177,7 +187,7 @@ class GenericGraph:
         lines = lines.read()
         numberOfVertices, lines = NextDecimalNumber(lines)
         for i in range(numberOfVertices):
-            node = Node(i)
+            node = Vertex(i)
             self.vertices.append(node)
         numberOfEdges, lines = NextDecimalNumber(lines)
         for i in range(numberOfEdges):
@@ -220,44 +230,39 @@ class GenericGraph:
     def TopologicalSort(self):
         pass
 
-    def Kruskal(self): #Returns a set of edges of the minimum spanning tree and the total length of the tree
+    def Kruskal(self): #Returns a list of edges of the minimum spanning tree
         edgesResult = []
-        subtrees = []
-        alreadyInSubtree = []
+        verticesOfsubtrees = []
         subtreesOfVertices = {}
         while(self.edges.NotEmpty()):
             edge = self.edges.Dequeue()            
-            if(edge.vertex1 not in alreadyInSubtree and edge.vertex2 not in alreadyInSubtree):
-                alreadyInSubtree.append(edge.vertex1)
-                alreadyInSubtree.append(edge.vertex2)
-                subtreesOfVertices[edge.vertex1] = len(subtrees)
-                subtreesOfVertices[edge.vertex2] = len(subtrees)
-                subtrees.append([edge.vertex1, edge.vertex2])
+            if(edge.vertex1 not in subtreesOfVertices and edge.vertex2 not in subtreesOfVertices):
+                subtreesOfVertices[edge.vertex1] = len(verticesOfsubtrees)
+                subtreesOfVertices[edge.vertex2] = len(verticesOfsubtrees)
+                verticesOfsubtrees.append([edge.vertex1, edge.vertex2])
                 edgesResult.append(edge)
-            elif(edge.vertex1 in alreadyInSubtree and edge.vertex2 not in alreadyInSubtree):
-                alreadyInSubtree.append(edge.vertex2)
-                subtrees[subtreesOfVertices[edge.vertex1]].append(edge.vertex2)
+            elif(edge.vertex1 in subtreesOfVertices and edge.vertex2 not in subtreesOfVertices):
+                verticesOfsubtrees[subtreesOfVertices[edge.vertex1]].append(edge.vertex2)
                 subtreesOfVertices[edge.vertex2] = subtreesOfVertices[edge.vertex1]
                 edgesResult.append(edge)
-            elif(edge.vertex1 not in alreadyInSubtree and edge.vertex2 in alreadyInSubtree):
-                alreadyInSubtree.append(edge.vertex1)
-                subtrees[subtreesOfVertices[edge.vertex2]].append(edge.vertex1)
+            elif(edge.vertex1 not in subtreesOfVertices and edge.vertex2 in subtreesOfVertices):
+                verticesOfsubtrees[subtreesOfVertices[edge.vertex2]].append(edge.vertex1)
                 subtreesOfVertices[edge.vertex1] = subtreesOfVertices[edge.vertex2]
                 edgesResult.append(edge)
             elif(subtreesOfVertices[edge.vertex1] != subtreesOfVertices[edge.vertex2]): #the subtree of vertex1 becomes part of the subtree of vertex2
-                previousSubtreeOfVertex1 = subtrees[subtreesOfVertices[edge.vertex1]]
+                previousSubtreeOfVertex1 = verticesOfsubtrees[subtreesOfVertices[edge.vertex1]]
                 for vertex in previousSubtreeOfVertex1:
-                    subtrees[subtreesOfVertices[edge.vertex2]].append(vertex)
+                    verticesOfsubtrees[subtreesOfVertices[edge.vertex2]].append(vertex)
                     subtreesOfVertices[vertex] = subtreesOfVertices[edge.vertex2]
                 previousSubtreeOfVertex1.clear()
                 edgesResult.append(edge)
 
         return edgesResult
 
-    def Jarnik(self): #Returns a set of edges of the minimum spanning tree and the total length of the tree
+    def Jarnik(self): #Returns a list of edges of the minimum spanning tree
         edgesResult = []
         edgesToTry = PriorityQueueShortestEdges()
-        verticesInTheSpanningTreeByIndex = [self.vertices[0]]
+        verticesInTheSpanningTreeByIndex = {self.vertices[0]}
         for edge in self.vertices[0].edges:
             edgesToTry.Enqueue(edge)
 
@@ -265,20 +270,105 @@ class GenericGraph:
             edgeToTry = edgesToTry.Dequeue()
             if(edgeToTry.vertex1 not in verticesInTheSpanningTreeByIndex):
                 edgesResult.append(edgeToTry)
-                verticesInTheSpanningTreeByIndex.append(edgeToTry.vertex1)               
+                verticesInTheSpanningTreeByIndex.add(edgeToTry.vertex1)               
                 for edge in self.vertices[edgeToTry.vertex1].edges:
                     edgesToTry.Enqueue(edge)
             elif(edgeToTry.vertex2 not in verticesInTheSpanningTreeByIndex):
                 edgesResult.append(edgeToTry)
-                verticesInTheSpanningTreeByIndex.append(edgeToTry.vertex2)
+                verticesInTheSpanningTreeByIndex.add(edgeToTry.vertex2)
                 for edge in self.vertices[edgeToTry.vertex2].edges:
                     edgesToTry.Enqueue(edge)
 
         return edgesResult
 
-    def Boruvka(self):
-        pass
+    def Boruvka(self): #Returns a set of edges of the minimum spanning tree
+        edgesResult = set()
+        edgesToTryOfSubtrees = [] #List of PriorityQueueShortestEdges
+        verticesOfsubtrees = []
+        subtreesOfVertices = {}
+        #First part, for each vertex, find the shortest edge and create subtrees from it
+        for vertex in self.vertices:
+            shortestEdge = vertex.ShortestEdge()
+            edgesResult.add(shortestEdge)
+            otherVertex = shortestEdge.TheIndexOfTheOtherVertex(vertex.index) 
+            if(vertex.index not in subtreesOfVertices and otherVertex not in subtreesOfVertices):
+                newSubtree = len(verticesOfsubtrees)
+                subtreesOfVertices[vertex.index] = newSubtree
+                subtreesOfVertices[otherVertex] = newSubtree
+                verticesOfsubtrees.append([vertex.index, otherVertex])
+                edgesToTryOfSubtrees.append(PriorityQueueShortestEdges())
+                for edge in vertex.edges:
+                    if(edge != shortestEdge):
+                        edgesToTryOfSubtrees[newSubtree].Enqueue(edge)
+                for edge in self.vertices[otherVertex].edges:
+                    if(edge != shortestEdge):
+                        edgesToTryOfSubtrees[newSubtree].Enqueue(edge)
+            elif((vertex.index not in subtreesOfVertices and otherVertex in subtreesOfVertices) or (vertex.index in subtreesOfVertices and otherVertex not in subtreesOfVertices)):    
+                isThere = vertex
+                notThere = self.vertices[otherVertex]
+                if(vertex.index not in subtreesOfVertices):
+                    isThere = notThere
+                    notThere = vertex
+                subtreesOfVertices[notThere.index] = subtreesOfVertices[isThere.index]
+                verticesOfsubtrees[subtreesOfVertices[isThere.index]].append(notThere.index)
+                for edge in notThere.edges:
+                    if(edge != shortestEdge):
+                        edgesToTryOfSubtrees[subtreesOfVertices[isThere.index]].Enqueue(edge)
+            elif(vertex.index in subtreesOfVertices and otherVertex in subtreesOfVertices and subtreesOfVertices[vertex.index]!=subtreesOfVertices[otherVertex]):
+                #the subtree of vertex becomes part of the subtree of otherVertex
+                oldSubtree = subtreesOfVertices[vertex.index]
+                newSubtree = subtreesOfVertices[otherVertex]
+                for vertex in verticesOfsubtrees[oldSubtree]:
+                    subtreesOfVertices[vertex] = newSubtree
+                    verticesOfsubtrees[newSubtree].append(vertex)
+                verticesOfsubtrees[oldSubtree].clear()
+                while(edgesToTryOfSubtrees[oldSubtree].NotEmpty()):
+                    edge = edgesToTryOfSubtrees[oldSubtree].Dequeue()
+                    if(edge.vertex1 != vertex or edge.vertex1 != otherVertex or edge.vertex2 != vertex or edge.vertex2 != otherVertex):
+                        #Cutting of edges, that connect two vertices of the newSubtree (those won't be used anymore)
+                        edgesToTryOfSubtrees[newSubtree].Enqueue(edgesToTryOfSubtrees[oldSubtree].Dequeue())
+        #Second part, until there is only one subtree (minimum spanning tree in this case), pick one not empty subtree, find the shortest edge, that is coming from it and connect that subtree with another one, that is connect to it by the edge
+        while(len(edgesResult)!=len(self.vertices)-1):
+            indexOfSubtree = 0
+            verticesOfsubtreesList = verticesOfsubtrees[indexOfSubtree]
+            while(verticesOfsubtreesList == []):
+                indexOfSubtree += 1
+                verticesOfsubtreesList = verticesOfsubtrees[indexOfSubtree]
+            shortestEdge = edgesToTryOfSubtrees[indexOfSubtree].Dequeue()
+            vertex = shortestEdge.vertex1
+            otherVertex = shortestEdge.vertex2
+            if(subtreesOfVertices[vertex] != subtreesOfVertices[otherVertex]): #It may be one of the edges, that is not relevant for me
+                NewSubtree = subtreesOfVertices[otherVertex]
+                OldSubtree = subtreesOfVertices[vertex]
+                edgesResult.add(shortestEdge)
+                #the subtree of vertex becomes part of the subtree of otherVertex
+                for vertex in verticesOfsubtrees[OldSubtree]:
+                    subtreesOfVertices[vertex] = NewSubtree
+                    verticesOfsubtrees[NewSubtree].append(vertex)
+                verticesOfsubtrees[OldSubtree].clear()
+                while(edgesToTryOfSubtrees[OldSubtree].NotEmpty()):
+                    edge = edgesToTryOfSubtrees[OldSubtree].Dequeue()
+                    if(edge.vertex1 != vertex or edge.vertex1 != otherVertex or edge.vertex2 != vertex or edge.vertex2 != otherVertex):
+                        #Cutting of edges, that connect two vertices of the NewSubtree (those won't be used anymore)
+                        edgesToTryOfSubtrees[NewSubtree].Enqueue(edgesToTryOfSubtrees[OldSubtree].Dequeue())
+        return edgesResult
 
+        # print(verticesOfsubtrees)
+        # print("Subtrees:")
+        # for vertex in subtreesOfVertices:
+        #     print(str(vertex)+" in "+str(subtreesOfVertices[vertex]))
+        # print()
+        # print("Edges in:")
+        # for edge in edgesResult:
+        #     print("from "+str(edge.vertex1)+" to "+str(edge.vertex2)+", length: "+str(edge.length))
+        # for i in range(len(edgesToTryOfSubtrees)):
+        #     print("Edges to try of the subtree "+str(i)+':')
+        #     while(edgesToTryOfSubtrees[i].NotEmpty()):
+        #         edge = edgesToTryOfSubtrees[i].Dequeue()
+        #         print("from "+str(edge.vertex1)+" to "+str(edge.vertex2)+", length: "+str(edge.length))
+        # for edge in edgesResult:
+        #     print("from "+str(edge.vertex1)+" to "+str(edge.vertex2)+", length: "+str(edge.length))
+                
 genericGraph = GenericGraph()
 genericGraph.CreateGraph("GenericInput.txt")
 #PrintGraph(genericGraph)
@@ -287,11 +377,11 @@ genericGraph.CreateGraph("GenericInput.txt")
 # print("From 7 to 2 the length is: "+str(length))
 # print("Path: "+str(path))
 
-kruskalEdges = genericGraph.Kruskal()
-PrintResultOfMinimumSpanningTree(kruskalEdges, "Kruskal")
+# kruskalEdges = genericGraph.Kruskal()
+# PrintResultOfMinimumSpanningTree(kruskalEdges, "Kruskal")
 
-jarnikEdges = genericGraph.Jarnik()
-PrintResultOfMinimumSpanningTree(jarnikEdges, "Jarnik")
+# jarnikEdges = genericGraph.Jarnik()
+# PrintResultOfMinimumSpanningTree(jarnikEdges, "Jarnik")
 
-
-
+# boruvkaEdges = genericGraph.Boruvka()
+# PrintResultOfMinimumSpanningTree(boruvkaEdges, "Boruvka")
